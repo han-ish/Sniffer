9e099521b949bacfa987406639ebc16c4ec6283f
han-ish <hanish0019@gmail.com>
2018-04-25 16:00:58 +0530

    Updated TCP header decoding

--- sniffer.c
+++ sniffer.c
@@ -41,6 +41,32 @@ struct sniff_ip {
 #define IP_HL(ip)               (((ip)->ip_vhl) & 0x0f)
 #define IP_V(ip)                (((ip)->ip_vhl) >> 4)
 
+
+/* TCP header */
+typedef u_int tcp_seq;
+
+struct sniff_tcp {
+        u_short th_sport;               /* source port */
+        u_short th_dport;               /* destination port */
+        tcp_seq th_seq;                 /* sequence number */
+        tcp_seq th_ack;                 /* acknowledgement number */
+        u_char  th_offx2;               /* data offset, rsvd */
+#define TH_OFF(th)      (((th)->th_offx2 & 0xf0) >> 4)
+        u_char  th_flags;
+        #define TH_FIN  0x01
+        #define TH_SYN  0x02
+        #define TH_RST  0x04
+        #define TH_PUSH 0x08
+        #define TH_ACK  0x10
+        #define TH_URG  0x20
+        #define TH_ECE  0x40
+        #define TH_CWR  0x80
+        #define TH_FLAGS        (TH_FIN|TH_SYN|TH_RST|TH_ACK|TH_URG|TH_ECE|TH_CWR)
+        u_short th_win;                 /* window */
+        u_short th_sum;                 /* checksum */
+        u_short th_urp;                 /* urgent pointer */
+};
+
 static void usage(char *argv[])
 {
 	char *prog = argv[0];
@@ -55,8 +81,11 @@ void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char
 	// declare pointers to packet headers
 	const struct sniff_ethernet *ethernet;	// The ethernet header
 	const struct sniff_ip *ip;	// The IP header
+	const struct sniff_tcp *tcp;	// The TCP header
 
 	int size_ip;
+	int size_tcp;
+	int size_payload;
 
 	// define ethernet header
 	ethernet = (struct sniff_ethernet *)(packet);
@@ -74,8 +103,49 @@ void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char
 	printf("    Ethernet destination address : %s\n", ether_ntoa((const struct ether_addr *)ethernet->ether_shost));
 	printf("    From : %s\n", inet_ntoa(ip->ip_src));
 	printf("    To   : %s\n", inet_ntoa(ip->ip_dst));
+
+	// determine protocol
+	switch(ip->ip_p) {
+		case IPPROTO_TCP:
+			printf("    Protocol: TCP\n");
+			break;
+		case IPPROTO_UDP:
+			printf("    Protocol: UDP\n");
+			return;
+		case IPPROTO_ICMP:
+			printf("    Protocol: ICMP\n");
+			return;
+		case IPPROTO_IP:
+			printf("    Protocol: IP\n");
+			return;
+		default:
+			printf("    Protocol: unknown\n");
+			return;
+	}
+
+	// Looks like we have a TCP packet
+	
+	// define/compute tcp header offset
+	tcp = (struct sniff_tcp *)(packet + SIZE_ETHERNET + size_ip);
+	size_tcp = TH_OFF(tcp)*4;
+	if (size_tcp < 20) {
+		printf("    * Invalid TCP header length : %u bytes\n", size_tcp);
+		return;
+	}
+
+	printf("    Src port : %d\n", ntohs(tcp->th_sport));
+	printf("    Dst port : %d\n", ntohs(tcp->th_dport));
+
+	// compute tcp payload (segment) size
+	size_payload = ntohs(ip->ip_len) - (size_ip + size_tcp);
+
+	if (size_payload > 0) {
+		printf("    Payload (%d bytes)\n", size_payload);
+	}
+
 	printf("\nPacket number %d:\n", count);
-	count++;
+	++count;
+	return;
 }
 
 
