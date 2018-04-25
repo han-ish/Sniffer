078b55713ec8c5c9d6f01d48920058ad23a4a4d9
han-ish <hanish0019@gmail.com>
2018-04-25 15:40:34 +0530

    Updated Ethernet and IP address decoding

--- sniffer.c
+++ sniffer.c
@@ -1,6 +1,45 @@
 #include <pcap.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <netinet/in.h>
+#include <netinet/ether.h>
+#include <arpa/inet.h>
+
+// define snap length (maximum bytes per packet to capture)
+#define SNAP_LEN 1518
+
+// ethernet headers are always exactly 14 bytes
+#define SIZE_ETHERNET 14
+
+// Ethernet addresses are 6 bytes
+#define ETHER_ADDR_LEN 6
+
+/* Ethernet header */
+struct sniff_ethernet {
+        u_char  ether_dhost[ETHER_ADDR_LEN];    /* destination host address */
+        u_char  ether_shost[ETHER_ADDR_LEN];    /* source host address */
+        u_short ether_type;                     /* IP? ARP? RARP? etc */
+};
+
+
+/* IP header */
+struct sniff_ip {
+        u_char  ip_vhl;                 /* version << 4 | header length >> 2 */
+        u_char  ip_tos;                 /* type of service */
+        u_short ip_len;                 /* total length */
+        u_short ip_id;                  /* identification */
+        u_short ip_off;                 /* fragment offset field */
+        #define IP_RF 0x8000            /* reserved fragment flag */
+        #define IP_DF 0x4000            /* dont fragment flag */
+        #define IP_MF 0x2000            /* more fragments flag */
+        #define IP_OFFMASK 0x1fff       /* mask for fragmenting bits */
+        u_char  ip_ttl;                 /* time to live */
+        u_char  ip_p;                   /* protocol */
+        u_short ip_sum;                 /* checksum */
+        struct  in_addr ip_src,ip_dst;  /* source and dest address */
+};
+#define IP_HL(ip)               (((ip)->ip_vhl) & 0x0f)
+#define IP_V(ip)                (((ip)->ip_vhl) >> 4)
 
 static void usage(char *argv[])
 {
@@ -8,6 +47,38 @@ static void usage(char *argv[])
 	printf("\nUsage\n%s <filter>\nexample : %s tcp\n", prog, prog);
 	exit(1);
 }
+
+void process_packet(u_char *args, const struct pcap_pkthdr *header, const u_char *packet)
+{
+	static int count = 1;                   /* packet counter */
+
+	// declare pointers to packet headers
+	const struct sniff_ethernet *ethernet;	// The ethernet header
+	const struct sniff_ip *ip;	// The IP header
+
+	int size_ip;
+
+	// define ethernet header
+	ethernet = (struct sniff_ethernet *)(packet);
+
+	// define/compute ip header offset
+	ip = (struct sniff_ip *)(packet + SIZE_ETHERNET);
+	size_ip = IP_HL(ip)*4;
+	if (size_ip < 20) {
+		printf("  * Invalid IP header length : %u bytes\n", size_ip);
+		return;
+	}
+
+	// print source and destination IP addresses
+	printf("    Ethernet source address : %s\n", ether_ntoa((const struct ether_addr *)ethernet->ether_dhost));
+	printf("    Ethernet destination address : %s\n", ether_ntoa((const struct ether_addr *)ethernet->ether_shost));
+	printf("    From : %s\n", inet_ntoa(ip->ip_src));
+	printf("    To   : %s\n", inet_ntoa(ip->ip_dst));
+	printf("\nPacket number %d:\n", count);
+	count++;
+}
+
+
 int main(int argc, char *argv[])
 {
 	pcap_t *handle;
@@ -19,6 +90,7 @@ int main(int argc, char *argv[])
 	bpf_u_int32 net;
 	struct pcap_pkthdr header;
 	const u_char *packet;
+	int num_packets = 10;			/* number of packets to capture */
 
 	if (argc < 2) {
 		fprintf(stderr, "Not enough arguments");
@@ -64,6 +136,10 @@ int main(int argc, char *argv[])
 	// Grab a packet
 	packet = pcap_next(handle, &header);
 	printf("Jacked a packet with length [%d]\n", header.len);
+
+	// capturing packets using callback
+	pcap_loop(handle, num_packets, process_packet, NULL);
+
 	pcap_close(handle);
 	fprintf(stdout, "This went well?\n");
 	return(0);
